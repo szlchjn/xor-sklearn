569fde5a8a72512dbc26cd238ce40f7f19eae54a
Jan Szołucha <szolucha.jan@gmail.com>
2018-04-06 16:51:37 +0200

    Add plot labels

--- xor-scikit.py
+++ xor-scikit.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 X = [[0, 0], [0, 1], [1, 0], [1, 1]]
 y = [0, 1, 1, 0]
 
-regr = MLPRegressor(hidden_layer_sizes=(4),
+regr = MLPRegressor(hidden_layer_sizes=(2),
                    activation='tanh',
                    solver='lbfgs')
 
@@ -20,9 +20,21 @@ for i in range(res):
         x = np.array([i/res, j/res]).reshape(1, -1)
         output[i][j] = model.predict(x)[0]
 
+
+
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111)
-plt.imshow(np.array(output), interpolation='nearest')
+cax = plt.imshow(np.array(output), interpolation='nearest', vmin=0, vmax=1)
+cbar = fig.colorbar(cax, ticks=[0, 1])
+cbar.ax.tick_params(labelsize=15)
 plt.set_cmap('gray')
-plt.colorbar(orientation='vertical')
+plt.axis('off')
+
+table = {'0, 0':(-2, -1),
+         '0, 1':(-2, res+2),
+         '1, 0':(res-2, -1),
+         '1, 1':(res-2, res+2)}
+for text, corner in table.items():
+    ax.annotate(text, xy=corner, size=15, annotation_clip=False)
+
 plt.show()
